c49d0ae42a94232e8c430a447b2dfd2580d7c646
Ahmed Alhasan's Youth <shebabahmedalhasan@gmail.com>
2022-08-12 04:20:39 +0430

    env

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,4 +1,4 @@
-export const AUTH_API = process.env.youth === 'development' ? 'http://localhost:1301/' : 'https://auth.ahmedalhasan.online/';
-export const DATA_API = process.env.youth === 'development' ? 'http://localhost:1302/' : 'https://data.ahmedalhasan.online/';
-export const CLOUD_API = process.env.youth === 'development' ? 'http://localhost:1304/' : 'https://cloud.ahmedalhasan.online/';
-export const OFFICIAL_API = process.env.youth === 'development' ? 'http://localhost:1303/' : 'https://office.ahmedalhasan.online/';
+export const AUTH_API = process.env.NODE_ENV === 'development' ? 'http://localhost:1301/' : 'https://auth.ahmedalhasan.online/';
+export const DATA_API = process.env.NODE_ENV === 'development' ? 'http://localhost:1302/' : 'https://data.ahmedalhasan.online/';
+export const CLOUD_API = process.env.NODE_ENV === 'development' ? 'http://localhost:1304/' : 'https://cloud.ahmedalhasan.online/';
+export const OFFICIAL_API = process.env.NODE_ENV === 'development' ? 'http://localhost:1303/' : 'https://office.ahmedalhasan.online/';
