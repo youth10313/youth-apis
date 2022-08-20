3bc426efdb7814b3ceb448eed8e079f5d54d71f4
Ahmed Alhasan's Youth <shebabahmedalhasan@gmail.com>
2022-08-21 01:53:18 +0430

    cleaning mistakes in reasons

--- src/apis/data/feeds/reasons/index.ts
+++ src/apis/data/feeds/reasons/index.ts
@@ -1,11 +1,10 @@
-import { Pagination } from "../../../../@types/common";
 import { Reason } from "../../../../@types/reason";
 import { Religion } from "../../../../@types/religion";
 import { DATA_API } from "../../../../constant/api";
 import { EntityRequest } from "../../../../models/entity-request";
 
-const Module1 = new EntityRequest<Reason, any, Pagination<Reason>>(DATA_API + 'reasons');
-const Module2 = new EntityRequest<Religion, any, Pagination<Religion>>(DATA_API + 'religions');
+const Module1 = new EntityRequest<Reason, any, Reason>(DATA_API + 'reasons');
+const Module2 = new EntityRequest<Religion, any, Religion>(DATA_API + 'religions');
 
 const ReasonsFeeds = Module1.Functions()
 const Religions = Module2.Functions()
