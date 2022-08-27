7e8c2d904557ca82132b37a9a57abc1a804f78f1
Ahmed Alhasan's Youth <shebabahmedalhasan@gmail.com>
2022-08-28 03:41:06 +0430

    teachers added to school

--- src/apis/data/feeds/school/index.ts
+++ src/apis/data/feeds/school/index.ts
@@ -1,20 +1,24 @@
 import { Class } from "../../../../@types/class";
 import { Pagination } from "../../../../@types/common";
 import { Lesson } from "../../../../@types/lesson";
+import { Teacher } from "../../../../@types/teacher";
 import { DATA_API } from "../../../../constant/api";
 import { EntityRequest } from "../../../../models/entity-request";
 import Chapters from "./chapters";
 
 const Module1 = new EntityRequest<Class, any, Pagination<Class>>(DATA_API + 'classes');
 const Module2 = new EntityRequest<Lesson, any, Pagination<Lesson>>(DATA_API + 'lessons');
+const Module3 = new EntityRequest<Teacher, any, Pagination<Teacher>>(DATA_API + 'teachers');
 
 
 const Classes = Module1.Functions();
 const Lessons = Module2.Functions();
+const Teachers = Module3.Functions();
 
 const School = {
     Classes,
     Lessons,
+    Teachers,
     Chapters
 }
 
