b536f9e20888951839fd975d69d4bafc22f59a52
Ahmed Alhasan's Youth <shebabahmedalhasan@gmail.com>
2022-08-31 14:15:08 +0430

    abort controller added

--- src/models/request.ts
+++ src/models/request.ts
@@ -7,9 +7,10 @@ export const YouthAPI = <T>(
     headers: any,
     body: any,
     onChange: (state: boolean) => void | any,
+    controller: AbortController,
     timeout = 1000 * 13) => new Promise<T>((resolve, reject) => {
         onChange(true)
-        const ax = method === 'get' || method === 'delete' ? axios[method](url, { headers, params, timeout }) : axios[method](url, body, { headers, params, timeout });
+        const ax = method === 'get' || method === 'delete' ? axios[method](url, { headers, params, timeout, signal: controller.signal }) : axios[method](url, body, { headers, params, timeout, signal: controller.signal });
         ax.then(res => {
             resolve(res.data as T);
             onChange(false)
@@ -26,9 +27,11 @@ export const YouthRoute = <T>(
     headers: any,
     body: any,
     timeout = 1000 * 13) => {
+    const controller = new AbortController();
     let complete: (data: T) => any
     let error: (err: any) => any
     let change: (state: boolean) => any
+    let abort = () => controller.abort()
     const obj = {
         onComelete: (func: (data: T) => any) => {
             complete = func;
@@ -43,10 +46,11 @@ export const YouthRoute = <T>(
             return obj
         },
         subscribe: () => {
-            YouthAPI<T>(method, url, params, headers, body, change, timeout)
+            YouthAPI<T>(method, url, params, headers, body, change, controller, timeout)
                 .then(res => complete(res))
                 .catch(err => error(err))
         },
+        abort
     }
     return obj;
 }
