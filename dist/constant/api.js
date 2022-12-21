00effc130e96c6906d62a986c8c051a90b9d4e27
Ahmed Alhasan's Youth <shebabahmedalhasan@gmail.com>
2022-12-21 09:58:40 +0330

    near to production

--- dist/constant/api.js
+++ dist/constant/api.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.OFFICIAL_API = exports.CLOUD_API = exports.DATA_API = exports.AUTH_API = void 0;
-exports.AUTH_API = process.env.AUTH_API_PROXY || 'https://auth.ahmedalhasan.online/';
-exports.DATA_API = process.env.DATA_API_PROXY || 'https://data.ahmedalhasan.online/';
-exports.CLOUD_API = process.env.CLOUD_API_PROXY || 'https://cloud.ahmedalhasan.online/';
-exports.OFFICIAL_API = process.env.OFFICIAL_API_PROXY || 'https://office.ahmedalhasan.online/';
+exports.AUTH_API = process.env.REACT_APP_AUTH_API_PROXY || 'https://auth.ahmedalhasan.online/';
+exports.DATA_API = process.env.REACT_APP_DATA_API_PROXY || 'https://data.ahmedalhasan.online/';
+exports.CLOUD_API = process.env.REACT_APP_CLOUD_API_PROXY || 'https://cloud.ahmedalhasan.online/';
+exports.OFFICIAL_API = process.env.REACT_APP_OFFICIAL_API_PROXY || 'https://office.ahmedalhasan.online/';
--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,4 +1,4 @@
-export const AUTH_API = process.env.AUTH_API_PROXY || 'https://auth.ahmedalhasan.online/';
-export const DATA_API = process.env.DATA_API_PROXY || 'https://data.ahmedalhasan.online/';
-export const CLOUD_API = process.env.CLOUD_API_PROXY || 'https://cloud.ahmedalhasan.online/';
-export const OFFICIAL_API = process.env.OFFICIAL_API_PROXY || 'https://office.ahmedalhasan.online/';
+export const AUTH_API = process.env.REACT_APP_AUTH_API_PROXY || 'https://auth.ahmedalhasan.online/';
+export const DATA_API = process.env.REACT_APP_DATA_API_PROXY || 'https://data.ahmedalhasan.online/';
+export const CLOUD_API = process.env.REACT_APP_CLOUD_API_PROXY || 'https://cloud.ahmedalhasan.online/';
+export const OFFICIAL_API = process.env.REACT_APP_OFFICIAL_API_PROXY || 'https://office.ahmedalhasan.online/';
