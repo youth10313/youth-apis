a984407b4195fdacce5882e109496a8d2b8edd08
Ahmed Alhasan's Youth <shebabahmedalhasan@gmail.com>
2023-05-12 18:00:02 +0330

    add by admin

--- src/apis/office/citizens/index.ts
+++ src/apis/office/citizens/index.ts
@@ -5,12 +5,13 @@ import { YouthRoute } from "../../../models/request";
 const CreateLink = (token = '') => YouthRoute<{ id: string, link: string }>('post', `${OFFICIAL_API}citizens/link`, {}, { token }, {});
 const DeleteLink = (token = '') => YouthRoute<{ msg: string }>('delete', `${OFFICIAL_API}citizens/link`, {}, { token }, {});
 const Join = (linkHash: string, Authorization = '') => YouthRoute('get', `${OFFICIAL_API}citizens/join/${linkHash}`, {}, { Authorization }, {});
+const JoinByAdmin = (linkHash: string, Authorization = '') => YouthRoute('get', `${OFFICIAL_API}citizens/join/by-admin/${linkHash}`, {}, { Authorization }, {});
 const Left = (id: string, Authorization = '') => YouthRoute('delete', `${OFFICIAL_API}citizens/join/${id}`, {}, { Authorization }, {});
 const ApproveCitizen = (userId: string, token = '') => YouthRoute('post', `${OFFICIAL_API}citizens/${userId}`, {}, { token }, {});
 const KickCitizen = (userId: string, token = '') => YouthRoute('delete', `${OFFICIAL_API}citizens/${userId}`, {}, { token }, {});
 const GetUserDepartments = (Authorization = '') => YouthRoute<Citizen[]>('get', `${OFFICIAL_API}citizens`, {}, { Authorization }, {});
 const GetDepartmentCitizens = (type: 'lobby' | 'all' | '', token = '') => YouthRoute<Citizen[]>('get', `${OFFICIAL_API}citizens`, { type }, { token }, {});
 
-const Citizens = { CreateLink, DeleteLink, Join, Left, ApproveCitizen, KickCitizen, GetUserDepartments, GetDepartmentCitizens };
+const Citizens = { CreateLink, DeleteLink, Join, Left, ApproveCitizen, KickCitizen, GetUserDepartments, GetDepartmentCitizens, JoinByAdmin };
 
 export default Citizens;
